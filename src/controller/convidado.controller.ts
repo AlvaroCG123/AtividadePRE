05ddc13227aeb6704a998becf5ec81bea8c7c85a
Alvaro Cantos Garcia <alvarocgarcia401@gmail.com>
2026-05-13 14:26:50 -0300

    erro de pasta

--- src/controller/convidado.controller.ts
+++ src/controller/convidado.controller.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from "express";
 import { prisma } from "../../lib/prisma.js";
 
+
 export async function getAll(req: Request, res: Response) {
     try{
         const list = await prisma
